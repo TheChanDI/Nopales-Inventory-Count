8329f8df395c7c5676c22bf8bfeda4e1718a6c3b
Prakash Chandra Awal <prakashchawal@Prakashs-MacBook-Pro.local>
2025-06-02 19:30:59 +1000

    mail functionality inprogress

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,8 +9,12 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import useInventoryStore from "@/store/useInventoryStore";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Linking, TouchableOpacity } from "react-native";
 
 export default function RootLayout() {
+  const inventory = useInventoryStore((state) => state.inventory);
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
@@ -21,6 +25,16 @@ export default function RootLayout() {
     return null;
   }
 
+  const handleOnPress = () => {
+    const formattedData = JSON.stringify(inventory, null, 2);
+    const email = "rcklsschandi@gmail.com";
+    const subject =
+      "Bar Inventory Count for " + Date().toString().split("T")[0];
+    const body = formattedData;
+    const mailtoURL = `mailto:${email}?subject=${subject}&body=${body}`;
+    Linking.openURL(mailtoURL).catch((err) => console.error("Error:", err));
+  };
+
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
@@ -29,6 +43,17 @@ export default function RootLayout() {
           options={{
             headerShown: true,
             headerTitle: "Nopales Inventory Count",
+            headerRight: () => (
+              <TouchableOpacity onPress={handleOnPress} activeOpacity={0.7}>
+                <AntDesign name="save" size={24} color="white" />
+              </TouchableOpacity>
+            ),
+            headerTitleStyle: {
+              color: "white",
+            },
+            headerStyle: {
+              backgroundColor: "#576056",
+            },
           }}
         />
         <Stack.Screen name="+not-found" />
